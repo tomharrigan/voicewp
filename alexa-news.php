5e0eec1acd011b298385960c914593766678b018
tomharrigan <tom29axp@gmail.com>
2017-02-07 00:25:33 -0500

    remove silly line

--- alexa-news.php
+++ alexa-news.php
@@ -99,8 +99,6 @@ class Alexa_News {
 			}
 		} elseif ( $request instanceof Alexa\Request\LaunchRequest ) {
 			$response->respond( __( 'Ask me whats up!', 'alexawp' ) );
-		} elseif ( $request instanceof Alexa\Request\SessionEndedRequest ) {
-			$response->respond( __( 'Thanks for listening!', 'alexawp' ) )->endSession();
 		}
 	}
 
