a4a89463f2defaf5f9beb71fb237fccd0505943d
tomharrigan <tom29axp@gmail.com>
2017-05-10 12:04:05 -0400

    field updates
    Combining separate items into single meta box on edit skill screen. Changes the prefix, will break back compat on fact/quote skills. requires resaving the app id if doing a standalone fact/quote skill.
    Fix a bug with saving empty trailing repeating field.

--- alexa-quote.php
+++ alexa-quote.php
@@ -7,7 +7,7 @@ class Alexa_Quote {
 		$response = $event->get_response();
 
 		$quotes_facts = get_post_meta( $id, 'facts_quotes', true );
-		$default_image = get_post_meta( $id, 'alexawp_default_image', true );
+		$default_image = get_post_meta( $id, 'alexawp_skill_default_image', true );
 		$quote_fact_index = rand( 0, count( $quotes_facts ) - 1 );
 		$quote_fact = $quotes_facts[ $quote_fact_index ];
 		$image = isset( $quote_fact['image'] ) ? $quote_fact['image'] : $default_image;
