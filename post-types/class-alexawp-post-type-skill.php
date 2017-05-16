adb89cb1602391688126e17a0e1af82888313ce5
tomharrigan <tom29axp@gmail.com>
2017-05-16 06:58:14 -0400

    first pass on saving an index intent array
    Need to handle removing items from array as well.

--- post-types/class-alexawp-post-type-skill.php
+++ post-types/class-alexawp-post-type-skill.php
@@ -11,6 +11,12 @@ class Alexawp_Post_Type_Skill extends Alexawp_Post_Type {
 	 */
 	public $name = 'alexawp-skill';
 
+	function __construct() {
+		parent::__construct();
+
+		add_filter( 'save_post', array( $this, 'skill_index' ), 11, 3 );
+	}
+
 	/**
 	 * Creates the post type.
 	 */
@@ -34,6 +40,35 @@ class Alexawp_Post_Type_Skill extends Alexawp_Post_Type {
 			'public' => true,
 			'supports' => array( 'title' ),
 		) );
+	}
+
+	/**
+	 * Handle saving an array of custom intents
+	 *
+	 * @param int $post_id Post ID
+	 * @param object $post Post object
+	 * @param bool $update Is post updated or new
+	 */
+	public function skill_index( $post_id, $post, $update ) {
+		if (
+			empty( $post_id )
+			|| wp_is_post_revision( $post_id )
+			|| ( defined( 'WP_IMPORTING' ) && WP_IMPORTING === true )
+			|| ( $post->post_type !== $this->name )
+		) {
+			return;
+		}
+
+		$is_standalone = get_post_meta( $post_id, 'alexawp_skill_is_standalone', true );
+		$skill_type = get_post_meta( $post_id, 'alexawp_skill_type', true );
+		if ( empty( $is_standalone ) && ! empty( $skill_type ) ) {
+			$custom_skill_index = get_option( 'alexawp_skill_index_map', array() );
+			$stuff = new Alexa_Quote;
+			foreach ( $stuff->intents as $intent ) {
+				$custom_skill_index[ $intent ] = $post_id;
+			}
+			update_option( 'alexawp_skill_index_map', $custom_skill_index );
+		}
 	}
 }
 
