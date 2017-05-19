0431960cdb65db19edbc293907e7fc898f5e125a
tomharrigan <tom29axp@gmail.com>
2017-05-19 11:24:08 -0400

    remove unneeded variable

--- alexa/skill/quote.php
+++ alexa/skill/quote.php
@@ -24,10 +24,9 @@ class Quote {
 	public function quote_request( $id, $request, $response ) {
 
 		$quotes_facts = get_post_meta( $id, 'facts_quotes', true );
-		$default_image = get_post_meta( $id, 'alexawp_skill_default_image', true );
 		$quote_fact_index = rand( 0, count( $quotes_facts ) - 1 );
 		$quote_fact = $quotes_facts[ $quote_fact_index ];
-		$image = isset( $quote_fact['image'] ) ? $quote_fact['image'] : $default_image;
+		$image = isset( $quote_fact['image'] ) ? $quote_fact['image'] : get_post_meta( $id, 'alexawp_skill_default_image', true );
 
 		$response->respond( $quote_fact['fact_quote'] )->withCard( $quote_fact['fact_quote'] . ' - ' . $quote_fact['attribution'], '', $image )->endSession();
 	}
