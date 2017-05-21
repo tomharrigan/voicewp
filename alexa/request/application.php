5cb216b606d4e67010e457127bcd36593e1c6a48
tomharrigan <tom29axp@gmail.com>
2017-05-21 12:37:24 -0400

    remove unneeded $requestApplicationId

--- alexa/request/application.php
+++ alexa/request/application.php
@@ -13,26 +13,19 @@ use InvalidArgumentException;
 
 class Application {
 	public $applicationId;
-	public $requestApplicationId;
 
 	public function __construct( $applicationId ) {
 		$this->applicationId = preg_split( '/,/', $applicationId );
 	}
 
-	public function setRequestApplicationId( $applicationId ) {
-		$this->requestApplicationId = $applicationId;
-	}
-
 	/**
-	 * Validate that the request Application ID matches our Application. This is required as per Amazon requirements.
+	 * Validate that the request Application ID matches our Application.
+	 * This is required as per Amazon requirements.
 	 *
 	 * @param $requestApplicationId
 	 * Application ID from the Request (typically found in $data['session']['application']
 	*/
 	public function validateApplicationId( $requestApplicationId = '' ) {
-		if ( empty( $requestApplicationId ) ) {
-			$requestApplicationId = $this->requestApplicationId;
-		}
 		if ( ! in_array( $requestApplicationId, $this->applicationId ) ) {
 			throw new InvalidArgumentException( 'Application Id not matched' );
 		}
