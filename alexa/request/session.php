2601439fe78502625b00437bddafaacb6c9baf8f
tomharrigan <tom29axp@gmail.com>
2017-05-22 00:24:18 -0400

    session_id snake case and get_attribute

--- alexa/request/session.php
+++ alexa/request/session.php
@@ -7,12 +7,12 @@ class Session {
 	public $user;
 	public $new;
 	public $application;
-	public $sessionId;
+	public $session_id;
 	public $attributes = array();
 
 	public function __construct( $data ) {
 		$this->user = new User( $data['user'] );
-		$this->sessionId = isset( $data['sessionId'] ) ? $data['sessionId'] : null;
+		$this->session_id = isset( $data['sessionId'] ) ? $data['sessionId'] : null;
 		$this->new = isset( $data['new'] ) ? $data['new'] : null;
 		if ( ! $this->new && isset( $data['attributes'] ) ) {
 			$this->attributes = $data['attributes'];
@@ -25,7 +25,7 @@ class Session {
 	 * @param mixed $default
 	 * @return mixed
 	 */
-	public function getAttribute( $key, $default = false ) {
+	public function get_attribute( $key, $default = false ) {
 		if ( array_key_exists( $key, $this->attributes ) ) {
 			return $this->attributes[ $key ];
 		} else {
