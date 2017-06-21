tomharrigan/voicewp

commit 2374b46dcf1ec8ce44c70f786264960bf2c9f02f

Flexible customization abilities (#71)
* filterable fields and params

* abstract validation

Moves logic to a common function that validates the request, and returns the request/response objects for use by skills

fail notice can exit early

* Remove meta query, add filters

removes meta query since a default value is set.
switch statement allows for customization in flash briefing items, and the response is now filterable

* remove debug garbage

* Add docs

* if request body empty, return fail notice

also add docs

* document a filter

class-voicewp.php
@@ -84,42 +84,57 @@ class Voicewp {
 	 * Get one item from the collection
 	 *
 	 * @param WP_REST_Request $request Full data about the request.
+	 * @param int $app_id The amazon app ID
+	 * @return JSON|array A JSON response is sent if there is an error
+	 * otherwise an array containing an alexa request and response object is returned
+	 */
+	public function voicewp_get_request_response_objects( $request, $app_id ) {
+		$body = $request->get_body();
+
+		$this->voicewp_maybe_display_notice();
+
+		if ( empty( $body ) ) {
+			$this->fail_response( new InvalidArgumentException( __( 'Request body is empty', 'voicewp' ) ) );
+		}
+
+		try {
+			$certificate = new \Alexa\Request\Certificate( $request->get_header( 'signaturecertchainurl' ), $request->get_header( 'signature' ), $app_id );
+			$alexa = new \Alexa\Request\Request( $body, $app_id );
+			$alexa->set_certificate_dependency( $certificate );
+
+			// Parse and validate the request.
+			$alexa_request = $alexa->from_data();
+
+		} catch ( InvalidArgumentException $e ) {
+			$this->fail_response( $e );
+		}
+
+		$response_object = new \Alexa\Response\Response;
+		$event = new AlexaEvent( $alexa_request, $response_object );
+
+		return array(
+			$event->get_request(),
+			$event->get_response(),
+		);
+	}
+
+	/**
+	 * Get one item from the collection
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
 	 * @return WP_Error|WP_REST_Response
 	 */
 	public function voicewp_skill_request( WP_REST_Request $request ) {
 
-		$this->voicewp_maybe_display_notice();
-
-		$body = $request->get_body();
-
 		$id = absint( $request->get_param( 'id' ) );
+		// get config based on url
+		$app_id = get_post_meta( $id, 'voicewp_skill_app_id', true );
 
-		if ( ! empty( $body ) ) {
-			try {
-				// get config based on url
-				//This ID will be made optional, first do a check for if standalone or not
-				$app_id = get_post_meta( $id, 'voicewp_skill_app_id', true );
-				$certificate = new \Alexa\Request\Certificate( $request->get_header( 'signaturecertchainurl' ), $request->get_header( 'signature' ), $app_id );
-				$alexa = new \Alexa\Request\Request( $body, $app_id );
-				$alexa->set_certificate_dependency( $certificate );
+		list( $request, $response ) = $this->voicewp_get_request_response_objects( $request, $app_id );
 
-				// Parse and validate the request.
-				$alexa_request = $alexa->from_data();
+		$this->skill_dispatch( $id, $request, $response );
 
-			} catch ( InvalidArgumentException $e ) {
-				return $this->fail_response( $e );
-			}
-
-			$response_object = new \Alexa\Response\Response;
-			$event = new AlexaEvent( $alexa_request, $response_object );
-
-			$request = $event->get_request();
-			$response = $event->get_response();
-
-			$this->skill_dispatch( $id, $request, $response );
-
-			return new WP_REST_Response( $response_object->render() );
-		}
+		return new WP_REST_Response( $response->render() );
 	}
 
 	/**
@@ -130,32 +145,16 @@ class Voicewp {
 	 */
 	public function voicewp_news_request( WP_REST_Request $request ) {
 
-		$this->voicewp_maybe_display_notice();
+		$alexa_settings = get_option( 'alexawp-settings' );
+		// The main amazon Application ID
+		$app_id = $alexa_settings['news_id'];
 
-		$body = $request->get_body();
+		list( $request, $response ) = $this->voicewp_get_request_response_objects( $request, $app_id );
 
-		if ( ! empty( $body ) ) {
-			try {
-				$alexa_settings = get_option( 'voicewp-settings' );
-				// The main amazon Application ID
-				$app_id = $alexa_settings['news_id'];
-				$certificate = new \Alexa\Request\Certificate( $request->get_header( 'signaturecertchainurl' ), $request->get_header( 'signature' ), $app_id );
-				$alexa = new \Alexa\Request\Request( $body, $app_id );
-				$alexa->set_certificate_dependency( $certificate );
+		$news = new \Alexa\Skill\News;
+		$news->news_request( $request, $response );
 
-				// Parse and validate the request.
-				$alexa_request = $alexa->from_data();
-			} catch ( InvalidArgumentException $e ) {
-				return $this->fail_response( $e );
-			}
-			$response = new \Alexa\Response\Response;
-			$event = new AlexaEvent( $alexa_request, $response );
-
-			$news = new \Alexa\Skill\News;
-			$news->news_request( $event );
-
-			return new WP_REST_Response( $response->render() );
-		}
+		return new WP_REST_Response( $response->render() );
 	}
 
 	/**
@@ -196,12 +195,12 @@ class Voicewp {
 	}
 
 	/**
-	 * In case of error, return response
+	 * In case of error, output JSON response and exit
 	 *
-	 * @return WP_REST_Response
+	 * @return JSON
 	 */
 	private function fail_response( $e ) {
-		return new WP_REST_Response(
+		wp_send_json(
 			array(
 				'version' => '1.0',
 				'response' => array(

alexa/skill/briefing.php
@@ -32,13 +32,6 @@ class Briefing {
 		// This logic could be both abstracted and used with array_map().
 		foreach (
 			get_posts( array(
-				'meta_query' => array(
-					array(
-						'key' => 'voicewp_briefing_source',
-						'compare' => '!=',
-						'value' => '',
-					),
-				),
 				'no_found_rows' => true,
 				'post_status' => 'publish',
 				'post_type' => 'voicewp-briefing',
@@ -55,7 +48,7 @@ class Briefing {
 				'redirectionUrl' => home_url(),
 			);
 
-			switch ( get_post_meta( $post->ID, 'voicewp_briefing_source', true ) ) {
+			switch ( $source = get_post_meta( $post->ID, 'voicewp_briefing_source', true ) ) {
 				case 'content' :
 					$response['mainText'] = $post->post_content;
 				break;
@@ -67,6 +60,20 @@ class Briefing {
 				case 'audio_url' :
 					$response['streamUrl'] = get_post_meta( $post->ID, 'voicewp_briefing_audio_url', true );
 				break;
+
+				default :
+					/**
+					 * Allows for including custom parameters within flash briefing items
+					 *
+					 * @since 1.1.0
+					 *
+					 * @param array $response A single briefing item
+					 * @param string $source The type of data populating this feed item
+					 * @param int $post->ID ID of post object
+					 * @param Object $post Post object
+					 */
+					$response = apply_filters( 'voicewp_briefing_source', $response, $source, $post->ID, $post );
+				break;
 			}
 
 			$response['mainText'] = wp_strip_all_tags( strip_shortcodes( $response['mainText'] ) );
@@ -74,6 +81,17 @@ class Briefing {
 			if ( isset( $response['streamUrl'] ) ) {
 				$response['streamUrl'] = esc_url_raw( $response['streamUrl'] );
 			}
+
+			/**
+			 * Allows for filtering a flash briefing item
+			 *
+			 * @since 1.1.0
+			 *
+			 * @param array $response A single briefing item
+			 * @param int $post->ID ID of post object
+			 * @param Object $post Post object
+			 */
+			$response = apply_filters( 'voicewp_briefing_response', $response, $post->ID, $post );
 
 			$responses[] = $response;
 		}

fields.php
@@ -74,57 +74,88 @@ function voicewp_fm_briefing_content() {
 	$allowed_formats = array( 'mp3' );
 
 	$children = array(
-		new \VoiceWP_Fieldmanager_Content_TextArea( __( 'Text', 'voicewp' ), array(
+		// Display-if control.
+		'source' => new \Fieldmanager_Radios( __( 'Source', 'voicewp' ), array(
+			/**
+			 * Allows for filtering the available sources that
+			 * can be used for populating a flash briefing
+			 *
+			 * @since 1.1.0
+			 *
+			 * @param array Flash briefing source options
+			 */
+			'options' => apply_filters( 'voicewp_briefing_source_options', array(
+				'content' => __( 'Text', 'voicewp' ),
+				'audio_url' => __( 'HTTPS URL to an MP3', 'voicewp' ),
+				'attachment_id' => __( 'Uploaded MP3', 'voicewp' ),
+			) ),
+			/**
+			 * String defining the default content source of a flash briefing
+			 *
+			 * @since 1.1.0
+			 *
+			 * @param string default value
+			 */
+			'default_value' => apply_filters( 'voicewp_default_briefing_source', 'content' ),
+		) ),
+		'content' => new \VoiceWP_Fieldmanager_Content_TextArea( __( 'Text', 'voicewp' ), array(
 			'description' => __( 'Text should be under 4,500 characters.', 'voicewp' ),
 			'attributes' => array(
 				'style' => 'width: 100%; height: 400px',
 				'maxlength' => 4500,
 			),
+			'display_if' => array(
+				'src' => 'source',
+				'value' => 'content',
+			),
 		) ),
-		new \Fieldmanager_Media( __( 'Uploaded MP3', 'voicewp' ), array(
-			'name' => 'attachment_id',
-			'mime_type' => 'audio/mpeg',
-			'button_label' => __( 'Select a File', 'voicewp' ),
-			'modal_button_label' => __( 'Select File', 'voicewp' ),
-			'modal_title' => __( 'Select a File', 'voicewp' ),
-		) ),
-		new \Fieldmanager_Link( __( 'HTTPS URL to an MP3', 'voicewp' ), array(
-			'name' => 'audio_url',
+		'audio_url' => new \Fieldmanager_Link( __( 'HTTPS URL to an MP3', 'voicewp' ), array(
 			'attributes' => array(
 				'style' => 'width: 100%;',
 			),
+			'display_if' => array(
+				'src' => 'source',
+				/**
+				 * Allow filtering of what sources an audio link is used with
+				 *
+				 * @since 1.1.0
+				 *
+				 * @param string Comma separated list of source options to display the field for
+				 */
+				'value' => apply_filters( 'voicewp_briefing_audio_url_display_if', 'audio_url' ),
+			),
 		) ),
+		'attachment_id' => new \Fieldmanager_Media( __( 'Uploaded MP3', 'voicewp' ), array(
+			'mime_type' => 'audio',
+			'button_label' => __( 'Select a File', 'voicewp' ),
+			'modal_button_label' => __( 'Select File', 'voicewp' ),
+			'modal_title' => __( 'Select a File', 'voicewp' ),
+			'display_if' => array(
+				'src' => 'source',
+				'value' => 'attachment_id',
+			),
+		) ),
+		'uuid' => new \Fieldmanager_Hidden( array() ),
 	);
 
-	foreach ( $children as $key => $value ) {
-		$children[ $key ]->display_if = array(
-			'src' => 'source',
-			'value' => $children[ $key ]->name,
-		);
-	}
-
-	// Display-if control.
-	$display_if = new \Fieldmanager_Radios( __( 'Source', 'voicewp' ), array(
-		'name' => 'source',
-		'options' => wp_list_pluck( $children, 'label', 'name' ),
-	) );
-
-	// Briefing UUID, saved the first time and used thereafter.
-	$uuid = new \Fieldmanager_Hidden( array(
-		'name' => 'uuid',
-	) );
-
 	if ( ! get_post_meta( $post_id, 'voicewp_briefing_uuid', true ) ) {
-		$uuid->default_value = voicewp_generate_uuid4();
+		$children['uuid']->default_value = voicewp_generate_uuid4();
 	}
 
-	array_unshift( $children, $display_if, $uuid );
+	/**
+	 * Allow addition, removal, or modification of briefing fields
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param array $children The Fieldmanager fields used with a flash briefing
+	 */
+	$children = apply_filters( 'voicewp_briefing_fields', $children );
 
 	$fm = new \Fieldmanager_Group( array(
 		'name' => 'voicewp_briefing',
 		'serialize_data' => false,
 		// Needs to be name => field for compat with FM's validation routines.
-		'children' => array_combine( wp_list_pluck( $children, 'name' ), $children ),
+		'children' => $children,
 	) );
 
 	// Help text.

commit 17a63c8c457d50fb428ebc2970269301387733d0

fix regression
Introduced a regression with https://github.com/alleyinteractive/voicewp/commit/2374b46dcf1ec8ce44c70f786264960bf2c9f02f#diff-6bb6e71efd2f554e830cd3797e88883bR148

class-voicewp.php
@@ -145,7 +145,7 @@ class Voicewp {
 	 */
 	public function voicewp_news_request( WP_REST_Request $request ) {
 
-		$alexa_settings = get_option( 'alexawp-settings' );
+		$alexa_settings = get_option( 'voicewp-settings' );
 		// The main amazon Application ID
 		$app_id = $alexa_settings['news_id'];
 