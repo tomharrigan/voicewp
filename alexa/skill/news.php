1b1627f2fb8d4aa78217b1177b5acfc62b6644a3
tomharrigan <tom29axp@gmail.com>
2017-11-01 00:54:56 -0400

    fix html entity parse issues
    having an `&nbsp;` for example, in the content, would break the reading of the output text and create an error

--- alexa/skill/news.php
+++ alexa/skill/news.php
@@ -174,7 +174,7 @@ class News {
 	public function format_single_post( $id, $single_post ) {
 		$voicewp_instance = \Voicewp_Setup::get_instance();
 		// Strip shortcodes and markup other than SSML
-		$post_content = preg_replace( '|^(\s*)(https?://[^\s<>"]+)(\s*)$|im', '', wp_kses( strip_shortcodes( $single_post->post_content ), $voicewp_instance::$ssml ) );
+		$post_content = html_entity_decode( preg_replace( '|^(\s*)(https?://[^\s<>"]+)(\s*)$|im', '', wp_kses( strip_shortcodes( $single_post->post_content ), $voicewp_instance::$ssml ) ) );
 		// Apply user defined dictionary to content as ssml
 		$dictionary = get_option( 'voicewp_user_dictionary', array() );
 		if ( ! empty( $dictionary ) ) {
